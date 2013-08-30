3d3cacb66146bd4c372c249e05b8b07b1d994ca2
funky81 <funky81.milis@gmail.com>
2013-08-31 06:58:12 +0700

    add vcc

--- examples/lcd_volt/lcd_volt.ino
+++ examples/lcd_volt/lcd_volt.ino
@@ -1,6 +1,127 @@
-void setup(){
-  
+//Sample using LiquidCrystal library
+#include <LiquidCrystal.h>
+ 
+/*******************************************************
+ 
+This program will test the LCD panel and the buttons
+Mark Bramwell, July 2010
+ 
+********************************************************/
+ 
+// select the pins used on the LCD panel
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+ 
+// define some values used by the panel and buttons
+int lcd_key     = 0;
+int adc_key_in  = 0;
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
+ 
+// read the buttons
+int read_LCD_buttons()
+{
+ adc_key_in = analogRead(0);      // read the value from the sensor 
+ // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+ // we add approx 50 to those values and check to see if we are close
+ if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+ // For V1.1 us this threshold
+ if (adc_key_in < 50)   return btnRIGHT;  
+ if (adc_key_in < 250)  return btnUP; 
+ if (adc_key_in < 450)  return btnDOWN; 
+ if (adc_key_in < 650)  return btnLEFT; 
+ if (adc_key_in < 850)  return btnSELECT;  
+ 
+ // For V1.0 comment the other threshold and use the one below:
+/*
+ if (adc_key_in < 50)   return btnRIGHT;  
+ if (adc_key_in < 195)  return btnUP; 
+ if (adc_key_in < 380)  return btnDOWN; 
+ if (adc_key_in < 555)  return btnLEFT; 
+ if (adc_key_in < 790)  return btnSELECT;   
+*/
+ 
+ 
+ return btnNONE;  // when all others fail, return this...
 }
-void loop(){
+ 
+void setup()
+{
+ lcd.begin(16, 2);              // start the library
+ lcd.setCursor(0,0);
+ lcd.print("Push the buttons"); // print a simple message
+}
   
+long readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high<<8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
+
+void loop()
+{
+ lcd.setCursor(9,1);            // move cursor to second line "1" and 9 spaces over
+ lcd.print(millis()/1000);      // display seconds elapsed since power-up
+ 
+ 
+ lcd.setCursor(0,1);            // move to the begining of the second line
+ lcd_key = read_LCD_buttons();  // read the buttons
+ 
+ switch (lcd_key)               // depending on which button was pushed, we perform an action
+ {
+   case btnRIGHT:
+     {
+     lcd.print("RIGHT ");
+     break;
+     }
+   case btnLEFT:
+     {
+     lcd.print("LEFT   ");
+     break;
+     }
+   case btnUP:
+     {
+     lcd.print("UP    ");
+     break;
+     }
+   case btnDOWN:
+     {
+     lcd.print("DOWN  ");
+     break;
+     }
+   case btnSELECT:
+     {
+     lcd.print("SELECT");
+     break;
+     }
+     case btnNONE:
+     {
+     lcd.print("NONE  ");
+     break;
+     }
+ }
+ 
 }
